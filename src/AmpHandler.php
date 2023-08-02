e68822c10cfa97a6b21c5bd7455c6037fe3664ce
Daniil Gentili <daniil@daniil.it>
2023-08-02 10:48:07 +0200

    Implement proxy and remaining options except decode_content and socks proxies

--- src/AmpHandler.php
+++ src/AmpHandler.php
@@ -4,13 +4,23 @@ namespace Amp\Http\Client\Psr7;
 
 use Amp\CancelledException;
 use Amp\DeferredCancellation;
+use Amp\Http\Client\Connection\DefaultConnectionFactory;
+use Amp\Http\Client\Connection\UnlimitedConnectionPool;
 use Amp\Http\Client\HttpClient;
 use Amp\Http\Client\HttpClientBuilder;
+use Amp\Http\Tunnel\Http1TunnelConnector;
+use Amp\Http\Tunnel\Https1TunnelConnector;
+use Amp\Socket\Certificate;
+use Amp\Socket\ClientTlsContext;
+use Amp\Socket\ConnectContext;
 use AssertionError;
 use GuzzleHttp\Promise\Promise;
 use GuzzleHttp\Promise\PromiseInterface;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Uri;
+use GuzzleHttp\RequestOptions;
+use GuzzleHttp\Utils;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
@@ -32,7 +42,11 @@ final class AmpHandler
         if (!\interface_exists(PromiseInterface::class)) {
             throw new AssertionError("Please require guzzle to use the guzzle AmpHandler!");
         }
-        $this->client = $client ?? HttpClientBuilder::buildDefault();
+        $this->client = $client ?? (
+            (new HttpClientBuilder)
+            ->followRedirects(0)
+            ->build()
+        );
         self::$psrAdapter ??= new PsrAdapter(new class implements RequestFactoryInterface {
             public function createRequest(string $method, $uri): RequestInterface
             {
@@ -56,14 +70,79 @@ final class AmpHandler
             }
             /** @psalm-suppress PossiblyNullReference Initialized in the constructor */
             $request = self::$psrAdapter->fromPsrRequest($request);
-            if (isset($options['timeout'])) {
-                $request->setTransferTimeout((float) $options['timeout']);
+            if (isset($options[RequestOptions::TIMEOUT])) {
+                $request->setTransferTimeout((float) $options[RequestOptions::TIMEOUT]);
             }
-            if (isset($options['connect_timeout'])) {
-                $request->setTcpConnectTimeout((float) $options['connect_timeout']);
+            if (isset($options[RequestOptions::CONNECT_TIMEOUT])) {
+                $request->setTcpConnectTimeout((float) $options[RequestOptions::CONNECT_TIMEOUT]);
+            }
+            if (isset($options[RequestOptions::PROXY])) {
+            }
+
+            $client = $this->client;
+            if (isset($options[RequestOptions::CERT]) ||
+                isset($options[RequestOptions::PROXY]) || (
+                    isset($options[RequestOptions::VERIFY])
+                    && $options[RequestOptions::VERIFY] !== true
+                )) {
+                $tlsContext = new ClientTlsContext();
+                if (isset($options[RequestOptions::CERT])) {
+                    if (\is_string($options[RequestOptions::CERT])) {
+                        $tlsContext = $tlsContext->withCertificate(new Certificate(
+                            $options[RequestOptions::CERT],
+                            $options[RequestOptions::SSL_KEY] ?? null,
+                        ));
+                    } else {
+                        $tlsContext = $tlsContext->withCertificate(new Certificate(
+                            $options[RequestOptions::CERT][0],
+                            $options[RequestOptions::SSL_KEY] ?? null,
+                            $options[RequestOptions::CERT][1]
+                        ));
+                    }
+                }
+                if (isset($options[RequestOptions::VERIFY])) {
+                    if ($options[RequestOptions::VERIFY] === false) {
+                        $tlsContext = $tlsContext->withoutPeerVerification();
+                    } else {
+                        $tlsContext = $tlsContext->withCaFile($options[RequestOptions::VERIFY]);
+                    }
+                }
+
+                $connector = null;
+                if (isset($options[RequestOptions::PROXY])) {
+                    if (!\is_array($options['proxy'])) {
+                        $connector = $options['proxy'];
+                    } else {
+                        $scheme = $request->getUri()->getScheme();
+                        if (isset($options['proxy'][$scheme])) {
+                            $host = $request->getUri()->getHost();
+                            if (!isset($options['proxy']['no']) || !Utils::isHostInNoProxy($host, $options['proxy']['no'])) {
+                                $connector = $options['proxy'][$scheme];
+                            }
+                        }
+                    }
+
+                    if ($connector !== null) {
+                        $connector = new Uri($connector);
+                        $connector = match ($connector->getScheme()) {
+                            'http' => new Http1TunnelConnector($connector->getHost().':'.$connector->getPort()),
+                            'https' => new Https1TunnelConnector($connector->getHost().':'.$connector->getPort(), new ClientTlsContext()),
+                        };
+                    }
+                }
+
+                $connectContext = (new ConnectContext)
+                    ->withTlsContext($tlsContext);
+
+                $client = (new HttpClientBuilder)
+                    ->usingPool(new UnlimitedConnectionPool(new DefaultConnectionFactory(connector: $connector, connectContext: $connectContext)))
+                    ->build();
+            }
+            if (isset($options['amp']['protocols'])) {
+                $request->setProtocolVersions($options['amp']['protocols']);
             }
             return self::$psrAdapter->toPsrResponse(
-                $this->client->request(
+                $client->request(
                     $request,
                     $cancellation
                 )
